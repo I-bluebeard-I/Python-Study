893ac3c96c0ad13351e034b95c84bb03d90f5ff8
Borodarus <_boroda_@mail.ru>
2021-02-28 14:18:54 +0300

    refactoring lesson01_task03

--- Lesson_1/part01_lesson01_task03.py
+++ Lesson_1/part01_lesson01_task03.py
@@ -14,8 +14,7 @@
 3) Написать алгоритм вычисляющий то же значение суммы, что и в пункте 2), но не создавая списков
 """
 
-sum = int()
-numbers_2 = []
+sum_total = 0
 
 # Создать список, состоящий из кубов нечётных чисел от 0 до 1000
 numbers = [num for num in range(1, 10 ** 3 + 1)]
@@ -27,38 +26,40 @@ numbers = list(filter(lambda x: x % 2,
 for number in numbers:                                  # для каждого числа в списке
     sum_of_digit = 0
     for digit in str(number):                           # для каждой цифры в числе
-        sum_of_digit = sum_of_digit + int(digit)        # считаем сумму цифр
-    if sum_of_digit % 7 == 0:                           # проверяем, что сумма цифр кратна 7
-        sum = sum + number
-print(f'1. {sum}')
+        sum_of_digit += int(digit)
+    if sum_of_digit % 7 == 0:
+        sum_total += number
+print(f'1. {sum_total}')
 
 # 2. К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из нового списка,
 # сумма цифр которых делится нацело на 7.
-sum = 0
+sum_total = 0
+numbers_2 = []
 
 for number in numbers:
     numbers_2.append(number + 17)
 for number in numbers_2:                                # для каждого числа в списке
     sum_of_digit = 0
     for digit in str(number):                           # для каждой цифры в числе
-        sum_of_digit = sum_of_digit + int(digit)        # считаем сумму цифр
-    if sum_of_digit % 7 == 0:                           # проверяем, что сумма цифр кратна 7
-        sum = sum + number
-print(f'2. {sum}')
+        sum_of_digit += int(digit)
+    if sum_of_digit % 7 == 0:
+        sum_total += number
+print(f'2. {sum_total}')
 
 # 3. Алгоритм вычисляющий то же значение суммы, что и в пункте 2), но не создавая списков
-sum = 0
-number = int()
+sum_total = 0
+number = 5
 
 while number < 1000:
-    if number % 2 == 0:                                 # если число четное = заглушка
-        pass
-    else:
-        cube_number = number ** 3 + 17                  # если число не четное возводим в куб + смещение 17
+    if number % 2 != 0:                                 # если число не четное возводим в куб + смещение 17
+        cube_number = number ** 3 + 17
         sum_of_digit = 0
-        for digit in str(cube_number):                  # для каждой цифры в числе
-            sum_of_digit = sum_of_digit + int(digit)    # считаем сумму цифр
-        if sum_of_digit % 7 == 0:                       # проверяем, что сумма цифр кратна 7
-            sum = sum + cube_number
+        while cube_number != 0:
+            sum_of_digit += (cube_number % 10)
+            cube_number //= 10
+        if sum_of_digit % 7 == 0:
+            sum_total += (number ** 3 + 17)
+    else:
+        pass
     number += 1
-print(f'3. {sum}')
+print(f'3. {sum_total}')
