4b6dfdc5f306dcb543be1d86e724e80e33e7eb78
Borodarus <_boroda_@mail.ru>
2021-02-28 13:02:12 +0300

    fix lesson01_task01

--- Lesson_1/part01_lesson01_task01.py
+++ Lesson_1/part01_lesson01_task01.py
@@ -13,27 +13,23 @@
 1 час 16 мин 7 сек
 """
 
-# Объявление переменных
-split_time = {'day': 0, 'hour': 0, 'min': 0, 'sec': 0}
+
 day_sec = 24 * 60 * 60
 hour_sec = 60 * 60
 min_sec = 60
 
-# Запрос интервала у пользователя
 user_sec = int(input('Введите размер интервала (в секундах): '))
 
-# Вычисление данных
-split_time['day'] = user_sec // day_sec
-split_time['hour'] = (user_sec - split_time['day'] * day_sec) // hour_sec
-split_time['min'] = (user_sec - split_time['day'] * day_sec - split_time['hour'] * hour_sec) // min_sec
-split_time['sec'] = user_sec - split_time['day'] * day_sec - split_time['hour'] * hour_sec - split_time['min'] * min_sec
+day = user_sec // day_sec
+hour = (user_sec - day * day_sec) // hour_sec
+minute = (user_sec - day * day_sec - hour * hour_sec) // min_sec
+sec = user_sec - day * day_sec - hour * hour_sec - minute * min_sec
 
-# Вывод результата
 if user_sec >= day_sec:
-    print(f"{split_time['day']} дн {split_time['hour']} час {split_time['min']} мин {split_time['sec']} сек")
+    print(f"{day} дн {hour} час {minute} мин {sec} сек")
 elif user_sec >= hour_sec:
-    print(f"{split_time['hour']} час {split_time['min']} мин {split_time['sec']} сек")
+    print(f"{hour} час {minute} мин {sec} сек")
 elif user_sec >= min_sec:
-    print(f"{split_time['min']} мин {split_time['sec']} сек")
+    print(f"{minute} мин {sec} сек")
 else:
-    print(f"{split_time['sec']} сек")
+    print(f"{sec} сек")
