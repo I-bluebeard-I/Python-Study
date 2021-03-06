f59f9c049d4d9df2cd65b5b4f1cc7700edf49bcf
Borodarus <_boroda_@mail.ru>
2021-03-06 17:43:16 +0300

    add lesson3.task5 solution

--- Lesson_3/part01_lesson03_task05.py
+++ Lesson_3/part01_lesson03_task05.py
@@ -5,11 +5,57 @@ nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
         Например:
->>> get_jokes(2)
+#>>> get_jokes(2)
 ["лес завтра зеленый", "город вчера веселый"]
 Документировать код функции.
 Сможете ли вы добавить еще один аргумент — флаг, разрешающий или запрещающий повторы слов в шутках (когда каждое
 слово можно использовать только в одной шутке)? Сможете ли вы сделать аргументы именованными?
-Задачи со * предназначены для продвинутых учеников, которым мало сделать обычное ДЗ.
 """
 
+
+def get_jokes(num_of_joke, *args):
+    # Документация
+
+    from random import randint
+    joke = []
+
+    for i in range(num_of_joke):
+        joke.append(f'{nouns[randint(0, len(nouns)-1)]} '
+                    f'{adverbs[randint(0, len(adverbs)-1)]} '
+                    f'{adjectives[randint(0, len(adjectives)-1)]}')
+    return joke
+
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+help(get_jokes)                                                         # добавить документацию
+num_of_joke = int(input('Сколько раз шутим? '))
+print(*get_jokes(num_of_joke, nouns, adverbs, adjectives), sep=', ')
+
+
+
+"""
+def get_jokes(num_of_joke, *args):
+
+    # Документация
+
+    from random import randint
+    joke = []
+
+    for i in range(num_of_joke):
+        joke.append(f'{nouns[randint(0, len(nouns)-1)]} '
+                    f'{adverbs[randint(0, len(adverbs)-1)]} '
+                    f'{adjectives[randint(0, len(adjectives)-1)]}')
+    return joke
+
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+help(get_jokes)                                                         # добавить документацию
+num_of_joke = int(input('Сколько раз шутим? '))
+print(*get_jokes(num_of_joke, nouns, adverbs, adjectives), sep=', ')
+"""
