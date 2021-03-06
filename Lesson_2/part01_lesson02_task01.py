d0d51de6994db14f68e9ad51ce1ff31cc6a6883c
Borodarus <_boroda_@mail.ru>
2021-03-06 17:51:13 +0300

    upd lesson2.task1 solution

--- Lesson_2/part01_lesson02_task01.py
+++ Lesson_2/part01_lesson02_task01.py
@@ -7,7 +7,7 @@
 """
 
 
-print('15 * 3', type(15 * 3))
-print('15 / 3', type(15 / 3))
-print('15 // 2', type(15 // 2))
-print('15 ** 2', type(15 ** 2))
+print(f'{type(15 * 3)= }')
+print(f'{type(15 / 3)= }')
+print(f'{type(15 // 2)= }')
+print(f'{type(15 ** 2)= }')
