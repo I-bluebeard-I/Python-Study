82a34d55f0324d21f02fe7f2e196ed5f37891a39
Borodarus <_boroda_@mail.ru>
2021-03-12 03:15:05 +0300

    add lesson4.task2 solution

--- Lesson_4/part01_lesson04_task02.py
+++ Lesson_4/part01_lesson04_task02.py
@@ -14,3 +14,35 @@ http://www.cbr.ru/scripts/XML_daily.asp.
 """
 
 
+def get_currency_rate(currency_char_code):
+
+    import requests
+
+    try:
+        page = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+
+        string_char_code = f'<CharCode>{currency_char_code.upper()}</CharCode>'
+        string_value_start = '<Value>'
+        string_value_end = '</Value>'
+
+        currency_char_code_position = page.text.find(string_char_code)
+        start_index = page.text.find(string_value_start, currency_char_code_position) + len(string_value_start)
+        stop_index = page.text.find(string_value_end, currency_char_code_position)
+
+        global currency_value
+        currency_value = page.text[start_index:stop_index]
+        currency_value = float('.'.join(currency_value. split(',')))
+
+    except:
+        currency_value = None
+
+    return currency_value
+
+
+currency_value = None
+
+currency_char_code = input('Введите код валюты (например, USD, EUR, GBP, ...): ')
+try:
+    print(f'{currency_char_code.upper()} {get_currency_rate(currency_char_code):4.2f}', type(currency_value))
+except:
+    print(get_currency_rate(currency_char_code), type(currency_value))
