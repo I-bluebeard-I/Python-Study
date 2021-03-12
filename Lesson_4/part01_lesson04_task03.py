c1c91d647130714cc0680ae53bb423de1ee61c48
Borodarus <_boroda_@mail.ru>
2021-03-12 15:26:39 +0300

    add lesson4.task3 solution

--- Lesson_4/part01_lesson04_task03.py
+++ Lesson_4/part01_lesson04_task03.py
@@ -7,3 +7,50 @@
 """
 
 
+def get_currency_rate(currency_char_code):
+
+    import requests
+    import datetime
+    from decimal import Decimal
+
+    try:
+        page = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+
+        global date_value
+
+        string_date = 'ValCurs Date="'
+        date_value_start = page.text.find(string_date) + len(string_date)
+        date_value_end = page.text.find(string_date) + len(string_date) + 10   # dd.mm.yyyy
+
+        date_value = page.text[date_value_start:date_value_end]
+        date_value = datetime.date(int(date_value[-4:]), int(date_value[3:5]), int(date_value[0:2]))
+
+        global currency_value
+
+        string_char_code = f'<CharCode>{currency_char_code.upper()}</CharCode>'
+        currency_value_start = '<Value>'
+        currency_value_end = '</Value>'
+
+        currency_char_code_position = page.text.find(string_char_code)
+        start_index = page.text.find(currency_value_start, currency_char_code_position) + len(currency_value_start)
+        stop_index = page.text.find(currency_value_end, currency_char_code_position)
+        currency_value = page.text[start_index:stop_index]
+        currency_value = Decimal(('.'.join(currency_value. split(','))))
+
+    except:
+        currency_value = None
+
+    return currency_value, date_value
+
+
+currency_value = None
+date_value = None
+
+currency_char_code = input('Введите код валюты (например, USD, EUR, GBP, ...): ')
+get_currency_rate(currency_char_code)
+
+try:
+    print(f'{currency_char_code.upper()} {currency_value:.2f}, {date_value}')
+    print(type(currency_value), type(date_value))
+except:
+    print(currency_value)
