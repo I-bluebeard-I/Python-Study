5337ca7bb7637c46a8a73d1ba6b2495022daaee3
Borodarus <_boroda_@mail.ru>
2021-03-12 17:51:25 +0300

    add lesson4.task5 solution

--- Lesson_4/part01_lesson04_task05.py
+++ Lesson_4/part01_lesson04_task05.py
@@ -10,4 +10,16 @@ USD 75.18, 2020-09-05
 EUR 80.35, 2020-09-05
 """
 
+# python part01_lesson04_task05.py usd eur gbp aud byr jpy brl hkd dkk
 
+import sys
+
+from utils import get_currency_rate
+
+
+for param in range(len(sys.argv)-1):
+
+    currency_char_code = sys.argv[param+1]
+    print(f'{currency_char_code.upper()} '
+          f'{get_currency_rate(currency_char_code)[0]}, '
+          f'{get_currency_rate(currency_char_code)[1]}')
