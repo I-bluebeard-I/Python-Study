9e8102d68c6930bbb8942d522ab96adae794877a
Borodarus <_boroda_@mail.ru>
2021-03-18 22:36:39 +0300

    upd lesson5.task5 solution

--- Lesson_5/part01_lesson05_task05.py
+++ Lesson_5/part01_lesson05_task05.py
@@ -8,17 +8,32 @@ result = [23, 1, 3, 10, 4, 11]
 """
 
 
+from time import perf_counter
+
 source = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 result = []
-flag = 0
+repeat = []
 
+start = perf_counter()
+for pos, num in enumerate(source):
+    repeat = list(1 for pos2, num2 in enumerate(source) if pos2 != pos and num == num2)
+    if len(repeat) == 0:
+        result.append(num)
+    repeat = []
+print(result, perf_counter() - start)       # 6,41936E-05 среднее время выполнения
+
+
+"""
+start = perf_counter()
 for pos, num in enumerate(source):
     for pos2, num2 in enumerate(source):
         if pos2 != pos:
             if num == num2:
-                flag = 1
+                repeat = 1
                 break
-    if flag == 0:
+    if repeat == 0:
         result.append(num)
-    flag = 0
+    repeat = 0
 print(result)
+print(perf_counter() - start)               # 7,6993E-05 среднее время выполнения
+"""
