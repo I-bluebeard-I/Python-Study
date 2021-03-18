511fc1f6e2a6eadcb98455a9680303053c889b2e
Borodarus <_boroda_@mail.ru>
2021-03-18 20:48:13 +0300

    add lesson5.task5 solution

--- Lesson_5/part01_lesson05_task05.py
+++ Lesson_5/part01_lesson05_task05.py
@@ -8,3 +8,17 @@ result = [23, 1, 3, 10, 4, 11]
 """
 
 
+source = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+result = []
+flag = 0
+
+for pos, num in enumerate(source):
+    for pos2, num2 in enumerate(source):
+        if pos2 != pos:
+            if num == num2:
+                flag = 1
+                break
+    if flag == 0:
+        result.append(num)
+    flag = 0
+print(result)
