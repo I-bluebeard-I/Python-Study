992108c8198d196c93497774da780f1944109891
Borodarus <_boroda_@mail.ru>
2021-03-22 01:24:26 +0300

    add lesson6.task1 solution

--- Lesson_6/part01_lesson06_task01.py
+++ Lesson_6/part01_lesson06_task01.py
@@ -14,3 +14,21 @@
 Примечание: код должен работать даже с файлами, размер которых превышает объем ОЗУ компьютера.
 """
 
+
+import json
+
+with open('nginx_logs.txt', 'r') as res_file:
+    while True:
+
+        with open('nginx_logs_parsed.txt', 'a') as parsed_file:
+            for line in res_file:
+
+                if not line:
+                    break
+
+                remote_addr = line[0:line.find(' ')]
+                request_type = line[line.find('"')+1:line.find(' ', line.index('"'))]
+                requested_resource = line[line.find('/', line.index('"')):line.find('HTTP')-1]
+
+                tmp_tuple = remote_addr, request_type, requested_resource
+                print(json.dumps(tmp_tuple), file=parsed_file)
