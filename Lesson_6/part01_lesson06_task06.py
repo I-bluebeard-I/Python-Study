0725bcf478dba5fbda25efaa13da7f38c3fb80c3
Borodarus <_boroda_@mail.ru>
2021-03-29 23:58:00 +0300

    upd lesson6.task6 solution

--- Lesson_6/part01_lesson06_task06.py
+++ Lesson_6/part01_lesson06_task06.py
@@ -28,3 +28,58 @@ python show_sales.py 1 3
 """
 
 
+# python part01_lesson06_task06.py show_sales 1573,7
+
+
+import sys
+
+if sys.argv[1].lower() == 'add_sale' and len(sys.argv) > 2 or\
+   sys.argv[1].lower() == 'show_sales' and len(sys.argv) > 4:
+    print('\nДля работы программы наберите:\n'
+          'PYTHON <имя скрипта.PY> ADD_SALE | SHOW_SALES [параметр_1] [параметр_2]\n\n'
+          'ADD_SALE [параметр_1]                      сохраняет данные о продажах\n'
+          'SHOW_SALES                                 выводить все записи о продажах\n'
+          'SHOW_SALES [параметр_1]                    выводит данные о продажах начиная с указанной позиции\n'
+          'SHOW_SALES [параметр_1] [параметр_2]       выводит данные о продажах в заданнром диапазоне')
+    exit(0)
+
+source_file = "data/bakery.csv"
+
+with open(source_file, 'a+', encoding='UTF-8') as source:
+
+    if sys.argv[1].lower() == 'add_sale':
+            print(sys.argv[2], file=source)
+
+    elif sys.argv[1].lower() == 'show_sales':
+
+        if len(sys.argv) == 2: # просто запуск скрипта — выводить все записи;
+            source.seek(0)
+            while True:
+                line = source.readline().strip()
+                print(line)
+                if not line:
+                    break
+
+        elif len(sys.argv) == 3: # запуск скрипта с одним параметром-числом —
+                                 # выводить все записи с номера, равного этому числу, до конца;
+            source.seek(0)
+            count = 1
+            while True:
+                line = source.readline().strip()
+                if count >= int(sys.argv[2]):
+                    print(line)
+                count += 1
+                if not line:
+                    break
+
+        elif len(sys.argv) == 4: # запуск скрипта с двумя числами — выводить записи, начиная с номера,
+                                 # равного первому числу, по номер, равный второму числу, включительно.
+            source.seek(0)
+            count = 1
+            while True:
+                line = source.readline().strip()
+                if int(sys.argv[2]) <= count <= int(sys.argv[3]):
+                    print(line)
+                count += 1
+                if not line:
+                    break
