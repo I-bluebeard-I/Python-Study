dc47c1b49b11c692b8dba00db8f4346246a704f1
Borodarus <_boroda_@mail.ru>
2021-03-30 14:07:13 +0300

    upd lesson7.task2 solution

--- Lesson_7/part01_lesson07_task02.py
+++ Lesson_7/part01_lesson07_task02.py
@@ -26,4 +26,44 @@
 """
 
 
+def create_structure():
 
+    import os
+
+    outdata_dir = 'data/2/'
+    config_file = 'data/config2.yaml'
+    level = {}
+    parent = []
+
+    with open(config_file, 'r', encoding='utf-8') as config:
+        string_count = 1
+        while True:
+            line = config.readline()
+            if not line:
+                break
+
+            level[string_count] = (round(line.count(' ') / 3), line.strip().replace('--', '').replace('|', '').strip())
+
+            if string_count > 1:
+                if level[string_count][0] > level[string_count - 1][0]:
+                    parent.append(level[string_count - 1][1])
+                elif level[string_count][0] < level[string_count - 1][0]:
+                    a = level[string_count - 1][0] - level[string_count][0]
+                    for i in range(a):
+                        parent.pop()
+
+            file_path = os.path.join(outdata_dir, *parent, level[string_count][1]).replace('\\', '/')
+
+            if level[string_count][1].find('.') != -1:
+                with open(file_path, 'a', encoding='utf-8'):
+                    pass
+            else:
+                try:
+                    os.makedirs(file_path)
+                except FileExistsError:
+                    pass
+
+            string_count += 1
+
+
+create_structure()
